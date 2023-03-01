7e0bdf5f4ce1106ca766fd420aed2a19a999c90c
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-01 23:01:47 +0530

    02-print.sh

---
+++ 02-print.sh
@@ -0,0 +1,13 @@
+echo Hello world
+
+# color Syntax
+# echo -e "\e[COL MESSAGE \e[0m"
+# echo -e -> Enable colors from echo command
+# \e[COLm -> Enable a particular Color
+# Color        Codes
+# red          31
+# green        32
+# yellow       33
+# blue         34
+#magenta       35
+#cyan          36
