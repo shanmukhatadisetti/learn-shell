7c61f000957a243d7ce9f30aef636a640aa5ac01
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-02 23:07:06 +0530

    06-conditions.sh

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -7,6 +7,8 @@ if [ "${number}" -eq 5 ]; then # then can go in next line as well, also semi col
 
   if [ "${string}" == abc ]; then
     echo string is abc
+  else
+    echo string is not abc
   fi
 
   ## It is always a good practice to quote the variables in expressions
