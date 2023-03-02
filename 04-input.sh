44c3677632f016abd37b4e18923c0ea6080a7751
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-02 22:36:09 +0530

    04-input.sh

--- 04-input.sh
+++ 04-input.sh
@@ -1,2 +1,11 @@
-echo $1
-echo $2
+# 0-n , $*/$@, $#
+
+echo First Argument $1 # first Argument
+echo Second Argument$2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+echo All Argument = $*
+echo All Argument = $@ #All Arguments
+
+echo Number of Argument = $# # Number of Arguments
