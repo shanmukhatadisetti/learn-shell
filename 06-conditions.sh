bfb5263a7df04045ff4db64e2751a80e3b3c3060
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-02 23:03:02 +0530

    06-conditions.sh

---
+++ 06-conditions.sh
@@ -0,0 +1,10 @@
+number=$1
+number=$2
+
+if [ ${number} -eq 5 ]; then # then can go in next line as well, also semi colan is also fine in the same line
+  echo number is 5
+  fi
+
+  if [ ${string} == abc ]; then
+    echo string is abc
+  fi
