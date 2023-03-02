ccde4fd144814059128cdde481316de1149b3820
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-02 23:05:19 +0530

    06-conditions.sh

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,10 +1,12 @@
 number=$1
 number=$2
 
-if [ ${number} -eq 5 ]; then # then can go in next line as well, also semi colan is also fine in the same line
+if [ "${number}" -eq 5 ]; then # then can go in next line as well, also semi colan is also fine in the same line
   echo number is 5
   fi
 
-  if [ ${string} == abc ]; then
+  if [ "${string}" == abc ]; then
     echo string is abc
   fi
+
+  ## It is always a good practice to quote the variables in expressions
