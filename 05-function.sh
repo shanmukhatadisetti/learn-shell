57ee76d6882b01a670e50045f735aa202ad64596
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-02 22:55:52 +0530

    05-function.sh

---
+++ 05-function.sh
@@ -0,0 +1,17 @@
+## Declare a function
+
+xyz() {
+  echo Hello from Function
+  echo First Argument - $1
+  echo Second Argument- $2
+  echo All Arguments  - $*
+  echo No of Arguments- $#
+  echo value of a = $2
+  b=200
+}
+
+## Main Program
+## Call a function
+a=120
+xyz 123 456
+echo Value of b - $b
