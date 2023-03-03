fcba6f7d32390a3ec0db1d41d5d927d27f5c0b5c
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-03 22:24:08 +0530

    08-case.sh

--- 08-case.sh
+++ 08-case.sh
@@ -18,3 +18,15 @@ case $name in
   *) echo No Course Available
     ;;
   esac
+
+
+## same above in if condition
+if [ "${name}" == "DevOPs" ]; then
+  echo Welcome to DevOps Training
+  echo Timings are 6AM
+  elif [ "${name}" == "AWS" ]; then
+    echo Welcome to aws Training
+    echo Timings are 7AM
+    else
+      echo No Course Availablr
+      fi
