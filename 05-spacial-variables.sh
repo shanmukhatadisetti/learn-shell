59a266ebe8dcc4417d5e1d634aba9f1406310fad
shanmukhatadisetti <shanmukhatadisetti@gmail.com>
2025-06-20 16:33:21 +0100

    added more content into hellowold.sh

--- 05-spacial-variables.sh
+++ 05-spacial-variables.sh
@@ -6,5 +6,5 @@ echo "current user directory is $PWD"
 echo "current user running the script is $USER"
 echo "home directory of the user is $HOME"
 echo "PID of the script is $$"
-sleep10
+sleep 10 &
 echo "PID of last command in background is $!"
