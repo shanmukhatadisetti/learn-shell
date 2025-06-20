e6c5817c170388196ff98be3445281b78af0b817
shanmukhatadisetti <shanmukhatadisetti@gmail.com>
2025-06-20 15:04:33 +0100

    added more content into hellowold.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,8 @@
-echo "james:: Today you have to work and you had shift at 10:00 to 7:00"
-echo "shan:: Sure i will be on time james"
-echo "james:: that's okay shan"
-echo "shan:: james if you dont mind can i do 9:30 to 6:30 because i had train at 6:50"
-echo "james:: ofcourse you can do shan. it's not a problem"
-echo "shan:: thanks james"
+PERSON1=james
+PERSON2=shan
+echo "$PERSON1:: Today you have to work and you had shift at 10:00 to 7:00"
+echo "$person2:: Sure i will be on time $PERSON1"
+echo "$PERSON1:: that's okay $person2"
+echo "$person2:: $PERSON1 if you dont mind can i do 9:30 to 6:30 because i had train at 6:50"
+echo "$PERSON1:: ofcourse you can do $person2. it's not a problem"
+echo "$person2:: thanks $PERSON1"
