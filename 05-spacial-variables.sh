4e589bffde57a3046311b1ba30f4160148b4233a
shanmukhatadisetti <shanmukhatadisetti@gmail.com>
2025-06-20 16:31:16 +0100

    added more content into hellowold.sh

---
+++ 05-spacial-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "all variables passed to the script-$@"
+echo "number of variables passed to the script-$#"
+echo "script name is $0"
+echo "current user directory is $PWD"
+echo "current user running the script is $USER"
+echo "home directory of the user is $HOME"
+echo "PID of the script is $$"
+sleep10
+echo "PID of last command in background is $!"
