5a2512a9e90623cdc10a288eb2661701d9f9b8be
shanmukhatadisetti <shanmukhatadisetti@gmail.com>
2025-06-23 19:52:35 +0100

    learning conditions

--- 08-colors.sh
+++ 08-colors.sh
@@ -8,18 +8,18 @@ N="\e[0m"
 USER=$(id -u)
 if [ $USER -ne 0 ]
 then 
-    echo -e $R"ERROR: You should run with Root User to Execute.$N"
+    echo -e "$R ERROR: You should run with Root User to Execute. $N"
     exit 1
 else
-    echo -e $G"ERROR: Your Running with Root User$N"
+    echo -e "$G ERROR: Your Running with Root User $N"
 fi
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e $G"$2 is SUCCESSFULLY Installed$N"
+        echo -e "$G $2 is SUCCESSFULLY Installed $N"
     else
-        echo -e $R"$2 is not SUCCESSFULLY Installed$N"
+        echo -e "$R $2 is not SUCCESSFULLY Installed $N"
         exit 1
     fi
 }
@@ -27,29 +27,29 @@ VALIDATE(){
 dnf list installed mysql
 if [ $? -ne 0 ]
 then 
-    echo -e $Y"INSTALLING MYSQL$N"
+    echo -e "$Y INSTALLING MYSQL $N"
     dnf install mysql -y
     VALIDATE $? "MySql"
 else
-    echo $Y"MYSQL Already installed no need to anything$N"
+    echo "$Y MYSQL Already installed no need to anything $N"
 fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]
 then 
-    echo $Y"INSTALLING Nginx$N"
+    echo "$Y INSTALLING Nginx $N"
     dnf install nginx -y
     VALIDATE $? "nginx"
 else
-    echo $Y"Nginx Already installed no need to anything$N"
+    echo "$Y Nginx Already installed no need to anything $N"
 fi
 
 dnf list installed python3
 if [ $? -ne 0 ]
 then 
-    echo $Y"INSTALLING python3$N"
+    echo "$Y INSTALLING python3 $N"
     dnf install python3 -y
     VALIDATE $? "python3"
 else
-    echo $Y"python3 Already installed no need to anything$N"
+    echo "$Y python3 Already installed no need to anything $N"
 fi
