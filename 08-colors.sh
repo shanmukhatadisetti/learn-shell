0ba4df28ccf228ec396b271c1b6fb56aa7986cc7
shanmukhatadisetti <shanmukhatadisetti@gmail.com>
2025-06-23 19:45:04 +0100

    learning conditions

--- 08-colors.sh
+++ 08-colors.sh
@@ -1,4 +1,55 @@
 #!/bin/bash
 
-echo -e "\e[31mError: File did not uploaded \e[0m"
-echo -e "\e[32mError: File has been uploaded \e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER=$(id -u)
+if [ $USER -ne 0 ]
+then 
+    echo -e $R"ERROR: You should run with Root User to Execute.$N"
+    exit 1
+else
+    echo -e $G"ERROR: Your Running with Root User$N"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e $G"$2 is SUCCESSFULLY Installed$N"
+    else
+        echo -e $R"$2 is not SUCCESSFULLY Installed$N"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "INSTALLING MYSQL"
+    dnf install mysql -y
+    VALIDATE $? "MySql"
+else
+    echo "MYSQL Already installed no need to anything"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "INSTALLING Nginx"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "Nginx Already installed no need to anything"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "INSTALLING python3"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 Already installed no need to anything"
+fi
