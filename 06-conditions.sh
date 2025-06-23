f138bfbc226a64dff0c0d7598c1c64b6b62cfa15
shanmukhatadisetti <shanmukhatadisetti@gmail.com>
2025-06-23 14:32:03 +0100

    learning conditions

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-NUMBER=$1
+# NUMBER=$1
 
-if [ $NUMBER -gt 10 ]
-then
-    echo "$NUMBER is greater than 10"
+# if [ $NUMBER -gt 10 ]
+# then
+#     echo "$NUMBER is greater than 10"
+# else
+#     echo "$NUMBER is less than 10"
+# fi
+
+USER=$(id -u)
+if [ $user -ne 0 ]
+then 
+    echo "ERROR: You should run with Root User to Execute."
 else
-    echo "$NUMBER is less than 10"
+    echo "ERROR: Your Running with Root User"
 fi
